f5680c4a374ec450eefc210023da4374a0ae88d2
Titan-69-hub <www.ansariarshan123@gmail.com>
2026-06-08 20:09:33 +0530

    Updated Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import StudentList from "./components/StudentList";
 import { Toaster } from "react-hot-toast";
 import "./index.css";
+
 function App() {
+
+  useEffect(() => {
+    axios.get(
+      "https://student-management-system-v7q0.onrender.com/api/students"
+    );
+  }, []);
+
   return (
     <div className="min-h-screen bg-base-200 p-6">
       <h1 className="text-3xl font-bold text-center mb-6">
@@ -10,6 +19,8 @@ function App() {
       </h1>
 
       <StudentList />
+
+      <Toaster position="top-right" />
     </div>
   );
 }
